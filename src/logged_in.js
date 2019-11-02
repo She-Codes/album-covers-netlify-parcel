fd0c4d0d051693b7f7cbfc073e23c2aee0b799d3
She-Codes <n.y.shaw@gmail.com>
2019-11-02 17:22:52 -0400

    logged out template in progress; log in button not currently working

---
+++ src/logged_in.js
@@ -0,0 +1,38 @@
+const renderLoggedInTemplate = () => {
+  document.body.classList.remove('logged-out');
+
+  document.getElementById('app').innerHTML = `
+  <nav class="navbar navbar-light bg-light justify-content-start">
+    <a class="navbar-brand" href="#">
+      Covers
+    </a>
+    
+    <form class="form-inline">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </nav>
+
+  <nav class="navbar navbar-dark bg-dark">
+    <form class="form-inline">
+      <select name="" id="" class="custom-select">
+        <option value="new releases">New Releases</option>
+        <option value="my covers">My Covers</option>
+      </select>
+    </form>
+  </nav>
+
+  <div class="content pt-4 mx-3">
+    <div class="album"></div>
+    <div class="album"></div>
+    <div class="album"></div>
+    <div class="album"></div>
+    <div class="album"></div>
+    <div class="album"></div>
+    <div class="album"></div>
+    <div class="album"></div>
+  </div>
+  `;
+}
+
+export default renderLoggedInTemplate;
